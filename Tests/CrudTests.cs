f1f6c15d2dec39436ee0bd9a00804cad0fb57ab8
Toby Davies <toby.davies@outlook.com>
2023-10-21 12:03:39 +0100

    more tests

--- Tests/CrudTests.cs
+++ Tests/CrudTests.cs
@@ -27,6 +27,32 @@ namespace Tests
             Assert.NotNull(fido);
         }
 
+        [Fact]
+        public async Task Catchers()
+        {
+            // Arrange
+            IDogCatcherDataService service = this.CreateSut();
+
+            // Act
+            CatcherContext gerald = await service.CreateCatcherAsync("gerald");
+
+            // Assert
+            Assert.NotNull(gerald);
+        }
+
+        [Fact]
+        public async Task Pounds()
+        {
+            // Arrange
+            IDogCatcherDataService service = this.CreateSut();
+
+            // Act
+            PoundContext dpo = await service.CreatePoundAsync("dog pound one");
+
+            // Assert
+            Assert.NotNull(dpo);
+        }
+
         private IDogCatcherDataService CreateSut()
             => new DogCatcherDataService(this.fixture.Context);
     }
